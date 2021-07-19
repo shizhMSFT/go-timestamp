ac6a27489ccf087f684d8c6e779b8b9c3a95747f
Shiwei Zhang <shizh@microsoft.com>
2021-07-19 19:24:24 +0800

    more oids

--- oid.go
+++ oid.go
@@ -7,7 +7,12 @@ import (
 )
 
 var DigestAlgorithmOIDs = map[digest.Algorithm]asn1.ObjectIdentifier{
-	digest.SHA256: asn1.ObjectIdentifier([]int{2, 16, 840, 1, 101, 3, 4, 2, 1}),
-	digest.SHA384: asn1.ObjectIdentifier([]int{2, 16, 840, 1, 101, 3, 4, 2, 2}),
-	digest.SHA512: asn1.ObjectIdentifier([]int{2, 16, 840, 1, 101, 3, 4, 2, 3}),
+	digest.SHA256: {2, 16, 840, 1, 101, 3, 4, 2, 1},
+	digest.SHA384: {2, 16, 840, 1, 101, 3, 4, 2, 2},
+	digest.SHA512: {2, 16, 840, 1, 101, 3, 4, 2, 3},
 }
+
+var (
+	OIDSignedData = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 7, 2}
+	OIDCTTSTInfo  = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 9, 16, 1, 4}
+)
